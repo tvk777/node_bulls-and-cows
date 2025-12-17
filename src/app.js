c9987ba0395a29ab48b056e6dc0a1bcd5c95cdd9
tvk777 <klimenko.tv7@gmail.com>
2025-12-17 18:35:04 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+/* eslint no-console: [,{ allow: ["warn", "log", "error"] }] */
 'use strict';
 
-// Write your code here
+const readline = require('readline');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const numberToGuess = generateRandomNumber();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const askUser = function runGame() {
+  rl.question('Guess a 4 digit number! ', (userInput) => {
+    if (checkIsValidUserInput(userInput)) {
+      rl.close();
+
+      return getBullsAndCows(Number(userInput), numberToGuess);
+    }
+    runGame();
+  });
+};
+
+askUser();
--- src/modules/checkIsValidUserInput.js
+++ src/modules/checkIsValidUserInput.js
@@ -1,3 +1,4 @@
+/* eslint no-console: [,{ allow: ["warn", "log", "error"] }] */
 'use strict';
 
 /**
@@ -9,7 +10,29 @@
  * @return {boolean} - True if the user input is valid, false otherwise
  */
 function checkIsValidUserInput(userInput) {
-  /* Write your code here */
+  const inputSet = new Set(userInput);
+
+  if (isNaN(Number(userInput)) || userInput.length !== 4) {
+    console.log('You should enter a 4-digit Number. Try again.');
+
+    return false;
+  }
+
+  if (Number(userInput[0]) === 0) {
+    console.log("Your number shouldn't start with 0. Try again.");
+
+    return false;
+  }
+
+  if (inputSet.size !== 4) {
+    console.log(
+      "Your number shouldn't contain any duplicate digits. Try again.",
+    );
+
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = {
